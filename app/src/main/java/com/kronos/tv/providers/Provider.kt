e4e58cd3ab34dac9496be8c0a52c57b7ada89041
noeljc7 <juceverson8@gmail.com>
2025-12-08 18:33:52 -0600

    Update Provider.kt

--- app/src/main/java/com/kronos/tv/providers/Provider.kt
+++ app/src/main/java/com/kronos/tv/providers/Provider.kt
@@ -1,12 +1,14 @@
 package com.kronos.tv.providers
 
-import com.kronos.tv.models.SourceLink
+import com.kronos.tv.models.SearchResult
+import com.kronos.tv.models.Episode
 
-// Modificamos la clase base para cumplir con tu interfaz antigua
+// Esta clase conecta el mundo JS con la interfaz KronosProvider
 abstract class Provider : KronosProvider {
     abstract val language: String
 
-    // Estos métodos son los nuevos (internos)
-    abstract suspend fun search(query: String): List<com.kronos.tv.models.SearchResult>
+    // Métodos internos JS
+    abstract suspend fun search(query: String): List<SearchResult>
+    abstract suspend fun loadEpisodes(url: String): List<Episode>
     abstract suspend fun loadStream(id: String, type: String): String?
 }
