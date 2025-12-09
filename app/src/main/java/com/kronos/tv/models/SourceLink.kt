73f6a52165d96be1d86aa5656b21787e8bbecd69
noeljc7 <juceverson8@gmail.com>
2025-12-09 10:59:07 -0600

    Update SourceLink.kt

--- app/src/main/java/com/kronos/tv/models/SourceLink.kt
+++ app/src/main/java/com/kronos/tv/models/SourceLink.kt
@@ -1,12 +1,10 @@
-package com.kronos.tv.providers
+package com.kronos.tv.providers // Ojo al paquete
 
 data class SourceLink(
     val name: String,
     val url: String,
     val quality: String,
     val language: String,
-    val iconUrl: String? = null,
     val isDirect: Boolean = false,
-    val requiresWebView: Boolean = false,
-    val provider: String = ""
+    val requiresWebView: Boolean = false
 )
