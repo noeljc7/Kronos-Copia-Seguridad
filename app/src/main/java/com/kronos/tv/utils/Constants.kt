301a1be433e799fb64081cc208bf5808b7acf0eb
noeljc7 <juceverson8@gmail.com>
2025-12-14 07:26:30 -0600

    Create Constants.kt

---
+++ app/src/main/java/com/kronos/tv/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.kronos.tv.utils
+
+object Constants {
+    // Nombre del archivo python (sin .py) que cargaremos por defecto
+    const val DEFAULT_PLUGIN_NAME = "sololatino"
+
+    // URL para descargar actualizaciones (Por ahora pon una dummy o tu repo real)
+    // Si no tienes repo, no importa, haremos un truco en el paso 2 para que no falle.
+    const val PROVIDER_REPO_URL = "https://raw.githubusercontent.com/usuario/repo/main/sololatino.py"
+}
